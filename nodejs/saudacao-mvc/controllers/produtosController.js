4e703f219dcc9fb7a30589a01d0e4acf161ab983
Guilherme Scandolara Selau <guilhermesjs9@gmail.com>
2025-10-28 10:25:11 -0300

    Versão Cadastrar Produtos

--- nodejs/saudacao-mvc/controllers/produtosController.js
+++ nodejs/saudacao-mvc/controllers/produtosController.js
@@ -1,3 +1,5 @@
+const produtosModel = require("../models/produtosModel");
+
 module.exports = {
     produtos: (req, res) => {
         res.sendFile('produtos.html', { root: './views' });
@@ -5,5 +7,14 @@ module.exports = {
 
     camisetas: (req, res) => {
         res.sendFile('camisetas.html', { root: './views' });
+    },
+
+    formCadastrar: (req, res) => {
+        res.sendFile('formCadastrar.html', { root: './views' });
+    },
+
+    cadastrar: (req, res) => {
+        const { descrição, id, quantidade, preco } = req.body;
+        res.send(produtosModel.gerarCadastro(descrição, id, quantidade, preco));
     }
 };
